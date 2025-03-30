b83e54cd681df225e247763e5cf024b88cdc2c29
Kevin <kevin.tranr@outlook.com>
2025-03-30 11:20:55 -0400

    updated About page

--- src/pages/page4.js
+++ src/pages/page4.js
@@ -20,125 +20,121 @@ function Page4() {
         >
           About
         </h1>
-        {/* <div className="w-full mb-4 md:mb-6">
-          <img
-            src={main}  
-            alt="Welcome Banner"
-            className="w-full h-[250px] md:h-[500px] object-cover mb-2 rounded" 
-          />
-          <p 
-            style={{ fontFamily: 'AfacadFlux, sans-serif' }} 
-            className="text-center text-gray-600"
-          >
-            Tobermory Summer 2022
-          </p>
-        </div> */}
         <p 
-          style={{ fontFamily: 'Roboto, sans-serif' }} 
-          className="text-left tracking-wide text-sm md:text-lg"
-        >
-          Hey There!<br/><br/>
+  style={{ fontFamily: 'Roboto, sans-serif' }} 
+  className="text-left tracking-wide text-sm md:text-lg"
+>
+  Hey there!<br /><br />
 
-          Welcome to my blog, I'm currently working as a&nbsp;
-          <a 
-            href="https://en.wikipedia.org/wiki/Numerical_control" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="relative text-black hover:text-black group"
-          >
-            <span className="relative inline-block">
-              CNC Machinist
-              <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
-            </span>
-          </a>
-          &nbsp; while working on my own projects and learning new things in the mean while. 
-          Send me a message about anything! I'd love to chat. <br/><br/>
-          
-          I've been machining since 2018 where I really developed a passion for automated systems and all the possibilities
-          that come with it. I went to Seneca College in 2021 for computer programming and&nbsp;
-          <a 
-            href="https://learner.mycreds.ca/r/sharelink/56633dab-b720-4e8f-be19-15cb97fdfee6/5d2e0692-6392-47ca-b2ad-3a55fd111cf3" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="relative text-black hover:text-black group"
-          >
-            <span className="relative inline-block">
-              graduated in April 2024
-              <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
-            </span>
-          </a>
-          . I've been working 
-          working primarily on technologies such as React, Node.js, and Python.  <br/><br/>
+  Welcome to my blog. I'm currently working as a&nbsp;
+  <a 
+    href="https://en.wikipedia.org/wiki/Numerical_control" 
+    target="_blank" 
+    rel="noopener noreferrer"
+    className="relative text-black hover:text-black group"
+  >
+    <span className="relative inline-block">
+      CNC Machinist
+      <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+    </span>
+  </a>
+  , while continuously exploring new ideas and developing personal projects in my spare time.
+  <br /><br />
 
-          This blog is a collection of my thoughts, tutorials, and news I shared as an individual. I may touch on
-          sports, machining tricks, and of course developer struggles.
-        </p>
+  My journey into tech began in 2018 through machining, where I discovered a deep interest in automation and systems thinking. I enrolled at&nbsp;
+  <a 
+    href="https://learner.mycreds.ca/r/sharelink/56633dab-b720-4e8f-be19-15cb97fdfee6/5d2e0692-6392-47ca-b2ad-3a55fd111cf3" 
+    target="_blank" 
+    rel="noopener noreferrer"
+    className="relative text-black hover:text-black group"
+  >
+    <span className="relative inline-block">
+      Seneca College
+      <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+    </span>
+  </a>
+  &nbsp;in 2021, where I studied Computer Programming and&nbsp;
+  <span className="relative inline-block">
+    graduated in April 2024
+    <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+  </span>
+  .
+  <br /><br />
 
-        <hr className="border-t-2 border-gray-300 my-8 w-[90%] mx-auto" />
+  These days, I’m focused on developing tools that combine&nbsp;
+  <span className="relative inline-block">
+    algorithmic trading
+    <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+  </span>
+  ,&nbsp;
+  <span className="relative inline-block">
+    data analytics
+    <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+  </span>
+  , and financial strategy. This blog is where I share thoughts, technical tutorials, and personal updates. You’ll find a mix of topics here—from software challenges to machining tricks to the occasional sports commentary.
+</p>
 
-        <p 
-          style={{ fontFamily: 'Roboto, sans-serif' }} 
-          className="text-left tracking-wide text-sm md:text-lg"
-        >
-          My current projects are:<br/><br/>
+<hr className="border-t-2 border-gray-300 my-8 w-[90%] mx-auto" />
 
-          <ul className="list-disc ml-6">
-            <li>
-              <a 
-                href="https://github.com/kevintine/Python-Trading-Bot" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="relative text-black hover:text-black group"
-              >
-                <span className="relative inline-block">
-                  Python Candlesticks Analysis Chart
-                  <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
-                </span>
-              </a>
-              &nbsp;: Able to identify any type of candlestick pattern on any stock during any timeframe. 
-            </li>
-            <li>
-              Currently building a full stack website for my employer using React, Node.js, Express, and Tailwind CSS.
-            </li>
-          </ul>
+<p 
+  style={{ fontFamily: 'Roboto, sans-serif' }} 
+  className="text-left tracking-wide text-sm md:text-lg"
+>
+  Current Projects:<br /><br />
+  <ul className="list-disc ml-6">
+    <li>
+      <a 
+        href="https://github.com/kevintine/Python-Trading-Bot" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="relative text-black hover:text-black group"
+      >
+        <span className="relative inline-block">
+          Python Stock Trading Bot
+          <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+        </span>
+      </a>
+      &nbsp;: Currently a backtesting enviroment for algorithmic trading. Able to run strategies and visualize results. 
+    </li>
+    <li>
+      <a 
+        href="https://github.com/kevintine/Fantasy-BBall-App" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="relative text-black hover:text-black group"
+      >
+        <span className="relative inline-block">
+          Yahoo Fantasy NBA Analysis
+          <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+        </span>
+      </a>
+      &nbsp;: Statistical tools to analyze player and trade value for fantasy sports. 
+    </li>
+    <li>
+      <a 
+        href="https://github.com/kevintine/Museum-of-Art-Collection-API" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="relative text-black hover:text-black group"
+      >
+        <span className="relative inline-block">
+          Metropolitan Museum of Art Website
+          <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+        </span>
+      </a>
+      &nbsp;: Pulls artwork from an external API to display in a gallery-style interface.
+    </li>
+  </ul>
 
-          <br/>
-          A few others:<br/><br/>
+  <br />
+  I’m currently working through some deployment issues with the&nbsp;
+  <span className="relative inline-block">
+    TA-Lib library
+    <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
+  </span>
+  &nbsp;used in my trading bot. If you're familiar with deploying Python apps that rely on compiled .whl files—or using Docker to simplify things—feel free to reach out!
+</p>
 
-          <ul className="list-disc ml-6">
-            <li>
-              <a 
-                href="https://github.com/kevintine/Fantasy-BBall-App" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="relative text-black hover:text-black group"
-              >
-                <span className="relative inline-block">
-                  Yahoo Fantasy NBA Analysis
-                  <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
-                </span>
-              </a>
-              &nbsp;: Teams and trade analysis based on player statistics. Hoping to win my league this year.
-            </li>
-            <li>
-              <a 
-                href="https://github.com/kevintine/Museum-of-Art-Collection-API" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="relative text-black hover:text-black group"
-              >
-                <span className="relative inline-block">
-                  Metropolitan Museum of Art Website
-                  <span className="absolute left-0 bottom-1 w-0 h-1 bg-red-400 w-full opacity-700"></span>
-                </span>
-              </a>
-              &nbsp;: Pulls art pieces from an external API to display on a React/Next.js site
-            </li>
-          </ul>
-
-          <br/>
-          I'm currently having issues deploying my Python Trading Bot online, issues with the TA-Lib library and the .whl file, let me know if you can help with that. I might have to pick up on how to use Docker.  
-        </p>
       </motion.div>
 
       <Footer/>
